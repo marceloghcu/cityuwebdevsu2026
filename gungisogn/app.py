fe4158f24eff410ea163f12cce0041d9f2b1d50a
SSG0901 <mnmc969t75@privaterelay.appleid.com>
2026-07-22 10:58:42 -0700

    Delete gungisogn directory

--- gungisogn/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/login')
-def login():
-    username = request.args.get('username')
-    password = request.args.get('password')
-    return f"username: {username} , password: {password}"
