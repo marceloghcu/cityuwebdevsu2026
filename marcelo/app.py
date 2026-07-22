267b6e65ca757c5d6638110a1a7823821c4d5d14
marceloghcu <guerrahahnmarcelo@cityu.edu>
2026-07-22 10:58:07 -0700

    Add files via upload

--- marcelo/app.py
+++ marcelo/app.py
@@ -1,1 +1,10 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/login')
+def login():
+    username = request.args.get("username")
+    password = request.args.get("password")
+    return f"username: {username}, password: {password}"
 
