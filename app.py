04f6be079c3b9f1772d08b262144b4628234c7ed
iagwos <teqwr1524@gmail.com>
2026-07-22 11:02:04 -0700

    Seongyongan

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route('/login')
 def login():
-    username = request.args.get('username')
-    password = request.args.get('password')
+    username = request.args.get("username")
+    password = request.args.get("password")
     return f"username: {username}, password: {password}"
-
