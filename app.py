06535ec1c72a52bdca9e8c1739f351b708de5230
zwoo119 <117463076+zwoo119@users.noreply.github.com>
2026-07-23 03:00:39 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/login')
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    return f"username: {username} , password: {password}"
