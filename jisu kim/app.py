f8bfc7bf64703b89f1490ab18d03864405c5a589
jisu2746 <kimjisu2746@gmail.com>
2026-07-28 02:45:05 +0900

    Add files via upload

--- jisu kim/app.py
+++ jisu kim/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/login')
 def login():
     username = request.args.get("username")
     password = request.args.get("password")
-    return "username: " + username + ", password: " + password
+    if username == "user1" and password == "pass1":
+        return "valid"
+    return "invalid"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
